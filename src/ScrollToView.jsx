3a8eead98cbaa5306f9341bbf460152846d12abb
Michael Jhon Navidad <81199287+mikee-pixel@users.noreply.github.com>
2026-06-03 19:52:42 +0800

    Remove the console.logs from ScrollToHash component.

--- src/ScrollToView.jsx
+++ src/ScrollToView.jsx
@@ -3,20 +3,18 @@ import { useLocation } from "react-router-dom";
 
 const ScrollToView = () => {
     const location = useLocation();
-
+    
     useEffect(() => {
-        if(location.hash) {
-            console.log(location);
+        if(location.hash){
             const element = document.getElementById(`${location.hash.replace("#", "")}`);
             console.log(element);
-            if(element) {
-                element.scrollIntoView({
-                    behavior: "smooth",
-                    block: "start",
-                })
-            }
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            })
         }
     }, [location])
+
     return null;
 }
 
